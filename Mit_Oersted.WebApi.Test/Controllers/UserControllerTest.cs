08d3746a0d6500f6e9d65536efa3dbb74fe4d473
Hermit <30859033+HermitTheViking@users.noreply.github.com>
2021-03-20 20:44:27 +0100

    Name change + unit test

---
+++ Mit_Oersted.WebApi.Test/Controllers/UserControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+
+namespace Mit_Oersted.WebApi.Test.Controllers
+{
+    [TestFixture]
+    [Category("IntegrationTest")]
+    public class UserControllerTest
+    {
+        private IFixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+        }
+
+
+    }
+}
